fb3bb00986b93f8b7fdc0ae9d8cd13ade446d5db
Chiye <huangchiye@gmail.com>
2017-03-26 14:25:06 -0700

    gps dir+dist

--- project.c
+++ project.c
@@ -2,22 +2,29 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include "lcd.h"
+#include <math.h>
+
 #define FOSC 7372800
 #define BAUD 9600
 #define MYUBRR FOSC/16/BAUD-1
+#define pi 3.14159265358979323846
 
 volatile unsigned char temp_recv = 0;
 volatile unsigned int count = 0;
 volatile unsigned char gps_data[150];
 unsigned char latitude[15];
 unsigned char longitude[15];
-
+float latitude_f, longitude_f;
+unsigned char direction[3];
 //strings to test lcd
 const unsigned char str1[] = ">> at328-5.c hi <<901234";
 const unsigned char str2[] = ">> USC EE459L <<78901234";
 
 const unsigned char lat[] = "LAT:";
 const unsigned char lon[] = "LONG:";
+const unsigned char dir[] = "DIR:";
+const unsigned char dist[] = "DIST:";
+
 
 /*
 LCD functions
@@ -59,14 +66,79 @@ void parse_gps()
 		}
 		i += 3;
 		latitude[k] = '\0';
+		latitude_f = atof(latitude);
 		k = 0;
 		
 		while (gps_data[i] != ',') {
 			longitude[k++] = gps_data[i++];
 		}
 		longitude[k] = '\0';
+		longitude_f = atof(longitude);
 	}
 }
+
+void find_direction(double peer_latitude, double peer_longitude)
+{
+	if (peer_latitude > latitude_f)
+		if (peer_longitude > longitude_f)
+		{
+			direction[0]= 'N';
+			direction[1]= 'W';
+		}
+		else 
+		{
+			direction[0]= 'N';
+			direction[1]= 'E';
+		}
+	else
+		if (peer_longitude > longitude_f)
+		{
+			direction[0]= 'S';
+			direction[1]= 'W';
+		}
+		else
+		{
+			direction[0]= 'S';
+			direction[1]= 'E';
+		}
+
+			direction[2]= '\0';
+}
+
+
+
+/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+/*::  Function prototypes                                           :*/
+/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+double deg2rad(double);
+double rad2deg(double);
+
+double find_distance(double lat1, double lon1, double lat2, double lon2) {
+  double theta, dist;
+  theta = lon1 - lon2;
+  dist = sin(deg2rad(lat1)) * sin(deg2rad(lat2)) + cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * cos(deg2rad(theta));
+  dist = acos(dist);
+  dist = rad2deg(dist);
+  dist = dist * 60 * 1.1515;
+  dist = dist * 1.609344; // Dist in KM
+ 
+  return (dist);
+}
+
+/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+/*::  This function converts decimal degrees to radians             :*/
+/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+double deg2rad(double deg) {
+  return (deg * pi / 180);
+}
+
+/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+/*::  This function converts radians to decimal degrees             :*/
+/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+double rad2deg(double rad) {
+  return (rad * 180 / pi);
+}
+
 
 void init_serial() 
 {
@@ -83,6 +155,8 @@ int main(void) {
     
     init_serial();
 	sei();
+	float peer_latitude = 42.9;
+	float peer_longitude = 90.9;
     while (1) {               // Loop forever
         
         if (count == 50) {
@@ -90,10 +164,20 @@ int main(void) {
         	//strout(0, (unsigned char *) recv_buf);
         	
         	parse_gps();
+        	find_direction(peer_latitude,peer_longitude);
+        	double distance = find_distance(latitude_f, longitude_f, peer_latitude, peer_longitude);
+        	char distance_c[10];
+        	sprintf(distance_c, "%f", distance);
         	strout(0, (unsigned char *) lat); //prints out "LAT:"
         	strout(0x40, (unsigned char *) lon); //prints out "LONG:"
         	strout(0x04, (unsigned char *) latitude); //prints out the actual value of latitude
         	strout(0x45, (unsigned char *) longitude); //prints out the actual value of longitude
+        	strout(0x14, (unsigned char *) dir); //prints out "DIR:"
+        	strout(0x18, (unsigned char *) direction); //prints out the direction
+        	strout(0x54, (unsigned char *) dist); //prints out "DIST:"
+        	strout(0x59, (unsigned char *) distance_c); //prints out the distance in KM
+
+
         	_delay_ms(500);
         	cmdout(1);
         	count = 0;
